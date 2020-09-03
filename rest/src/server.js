62b26cc1b3b75b1694fe453810f875c12f83b41d
Max McMurray <mcmurrmax@ecs.vuw.ac.nz>
2020-09-03 17:35:44 +1200

    Make deployment of rest server through git

--- rest/src/server.js
+++ rest/src/server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
+const MongoClient = mongodb.MongoClient;
 var itemManager = require('./item');
 var HATS_COLLECTION = 'hats';
 
@@ -10,8 +11,12 @@ app.use(bodyParser.json());
 
 var db;
 
+// For accessing the live database locally
+// process.env.MONGODB_URI = "mongodb+srv://herokuRestNode:KnND571lRn10cZDk@nwen304-shop-db.f9hmb.mongodb.net/store?retryWrites=true&w=1";
+
 // Connect to the database
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+let client = new MongoClient(process.env.MONGODB_URI || "mongodb://localhost:27017/test", { useNewUrlParser: true, useUnifiedTopology:true, authMechanism: 'SCRAM-SHA-1' });
+client.connect(err => {
   if (err) {
     console.log(err);
     process.exit(1);
