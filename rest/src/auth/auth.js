f966356b6e2db3cf8fdc1ed2b3de34bd68079409
Max McMurray <mcmurrmax@ecs.vuw.ac.nz>
2020-10-01 13:48:08 +1300

    Add password complexity requirement

--- rest/src/auth/auth.js
+++ rest/src/auth/auth.js
@@ -3,6 +3,12 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/auth.config');
 const { Error } = require('../error/CustomMongoError');
 
+// At least one uppercase, one lowercase and one number with a min length
+const passwordMinLength = 8;
+const passwordValidRegex = new RegExp('^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{' + passwordMinLength + ',}$');
+
+const isPasswordComplex = pass => passwordValidRegex.test(pass);
+
 const invalidCredentials = (resolve, reject) => {
   reject(Error("CredentialsNotGiven"));
 }
@@ -12,7 +18,7 @@ const validateUser = function (email, pass, token) {
     pass = email.password;
     token = email.token;
     email = email.email;
-  } 
+  }
 
   // Token validation
   if (token) {
@@ -42,3 +48,4 @@ const validateUser = function (email, pass, token) {
 }
 
 exports.validateUser = validateUser;
+exports.isPasswordComplex = isPasswordComplex;
