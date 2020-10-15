4f11d65796c51f7841a8c8c76ed4c8faf03de4c3
Max McMurray <mcmurrmax@ecs.vuw.ac.nz>
2020-10-15 15:45:35 +1300

    Get login working by showing name when logged in
    peer programing with Michael and Georgia

--- webapp/login/js/loginController.js
+++ webapp/login/js/loginController.js
@@ -1,9 +1,26 @@
-window.addEventListener("load",init);
-function init(){
-    document.querySelector('#loginBtn').addEventListener('click',loginAction);
-    document.querySelector('#forgotPassword').addEventListener('click',forgotPassAction);
-    document.querySelector('#signUp').addEventListener('click',signUpAction);
-    document.querySelector('#googleLoginBtn').addEventListener('click',loginGoogleAction);
+window.addEventListener("load", init);
+
+let loginButton;
+
+function init() {
+    let user = getCookie("token");
+
+    console.table([['user', user]]);
+
+    if (user) {
+        setTimeout(() => console.log("User logged in - redirecting"), 0);
+        document.location.href = '/';
+
+    } else {
+        loginButton = document.getElementById('loginBtn');
+        loginButton.addEventListener('click', loginAction);
+        document.querySelector('#forgotPassword').addEventListener('click', forgotPassAction);
+        document.querySelector('#signUp').addEventListener('click', signUpAction);
+        document.querySelector('#googleLoginBtn').addEventListener('click', loginGoogleAction);
+    }
+
+
+
 }
 
 function forgotPassAction() {
@@ -14,8 +31,11 @@ function signUpAction() {
     console.log("Sign up press")
 }
 
-function loginAction() {
-  console.log("Login press")
+async function loginAction() {
+    console.log("Login press")
+
+    loginButton.disabled = true;
+
     // let loginUrl = 'http://localhost:8080/api/auth/login'
     let loginUrl = 'https://limitless-cove-65021.herokuapp.com/api/auth/login';
     let email = document.querySelector("#login").value
@@ -33,15 +53,52 @@ function loginAction() {
         method: 'POST',
         headers: headers,
         redirect: 'follow',
-        body: "{'email': 'test@test'}"
     };
 
-    fetch(loginUrl, requestOptions)
-      .then(res=>res.json())
-      .then(res => console.log("login result", res))
-      .catch(err => console.log("error", err))
+    await fetch(loginUrl, requestOptions)
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+            throw { message: "ahhhhhhhhh" }
+        })
+        .then(res => {
+            setCookie([['token', res.access_token]]);
+            setCookie([['name', res.name]]);
+            document.location.href = '/';
+        })
+        .catch(err => console.log("error", err))
+
+    loginButton.disabled = false;
     // console.log(email)
     // console.log(password)
+}
+
+function setCookie(cookieValues) {
+    var d = new Date();
+    d.setTime(d.getTime() + (60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+
+    let cookies = "";
+    cookieValues.forEach(item => {
+        cookies = cookies + item[0] + '=' + item[1] + ';';
+        document.cookie = cookies + expires + ";path=/";
+    });
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 
 const CLIENT_ID = "742891582769-kve9i6a70635sc5lpme2hajavihogu17.apps.googleusercontent.com";
