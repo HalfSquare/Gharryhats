07a7acf4e0f9617b2818b60d20e7ce8bda8b3962
GStrongman <georgia.strongman@gmail.com>
2020-10-19 22:39:25 +1300

    Peer programming with Max cart is working

--- webapp/js/pageGetters/getCart.js
+++ webapp/js/pageGetters/getCart.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const dbOp = require('../dbOperations');
 const dbFun = require('../dbFunctions');
 
-function getCart(req, res, filename, cart) {
+async function getCart(req, res, filename, cart) {
     cart = JSON.parse(cart);
      console.log(cart);
-    fs.readFile(filename, function (error, data) {
+    fs.readFile(filename, async function (error, data) {
         if (error) {
             console.log(error);
             res.writeHead(404, { 'Content-Type': 'text/html' });
@@ -19,21 +20,42 @@ function getCart(req, res, filename, cart) {
                     <tr>
                         <th scope="col"></th>
                         <th scope="col">Item</th>
-                        <th scope="col">Quantity</th>
+                        <th scope="col">Price</th>
                         <th scope="col">Delete</th>
                     </tr>
                     </thead><tbody>`
+
+                    for (let item of cart){
+                        console.log(item);
+                        await dbOp.getItem(item)
+                        .then(res => {
+                            console.log(res);
+                            res = res[0];
+                            cartHtml += `
+                            <tr>
+                            <th scope="row"></th>
+                            <td>${res.name} for ${res.animal}</td>
+                            <td>$${res.price}</td>
+                            <td><button>Delete</button></td>
+                            </tr>
+                            `
+                        ;})
+                    }
                 
-                    cart.forEach(item => {
-                    cartHtml += `
-                        <tr>
-                        <th scope="row"></th>
-                        <td>${JSON.stringify(item)}</td>
-                        <td>1</td>
-                        <td><button>Delete</button></td>
-                        </tr>
-                        `;
-                })
+                    // cart.forEach(item => {
+                    // (dbOp.getItem(item)
+                    //     .then(res => {
+                    //         cartHtml += `
+                    //         <tr>
+                    //         <th scope="row"></th>
+                    //         <td>${JSON.stringify(res)}</td>
+                    //         <td>1</td>
+                    //         <td><button>Delete</button></td>
+                    //         </tr>
+                    //         `
+                    //     ;}));
+                        // console.log(cartHtml);
+                    
                 cartHtml += `</tbody></table>`
             } else {
                 cartHtml = "<p>No Items in Cart</p>"
